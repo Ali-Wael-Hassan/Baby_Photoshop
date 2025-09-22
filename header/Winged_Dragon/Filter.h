1895ff009da596c8e34faecdde3e7ffb9c4b8a10
Ali Hassan <aliwael01243@gmail.com>
2025-09-22 16:28:31 +0300

    Fix pointer to reference

--- header/Winged_Dragon/Filter.h
+++ header/Winged_Dragon/Filter.h
@@ -26,16 +26,16 @@ public:
     // Destructor
     ~Filter() = default;
 
-    void grayScale(Image* orig) override; // Grayscale Conversion
-    void blackWhite(Image* orig) override; // Black and White
-    void invertImage(Image* orig) override; // Invert Images
-    void mergeImage(Image* orig, int option, int transpaerncy) override; // Merge Images
-    void flipImage(Image* orig, int option) override; // Flip Image
-    void rotateImage(Image* orig, int degree) override; // Rotate Image
-    void darkenLightn(Image* orig, int percent) override; // Darken and Lighten Image
-    void cropImage(Image* orig, std::pair<int,int> st, std::pair<int,int> end) override; // Crop Image
-    void addFrame(Image* orig, Image* frame) override; // Adding a Frame to the Picture
-    void detectEdges(Image* orig) override; // Detect Image Edges
-    void resizeImage(Image* orig, int width, int height) override; // resize image
-    void blurImage(Image* orig, int radious) override; // Blur Image
+    void grayScale(Image &orig) override; // Grayscale Conversion
+    void blackWhite(Image &orig) override; // Black and White
+    void invertImage(Image &orig) override; // Invert Images
+    void mergeImage(Image &orig, int option, int transpaerncy) override; // Merge Images
+    void flipImage(Image &orig, int option) override; // Flip Image
+    void rotateImage(Image &orig, int degree) override; // Rotate Image
+    void darkenLightn(Image &orig, int percent) override; // Darken and Lighten Image
+    void cropImage(Image &orig, std::pair<int,int> st, std::pair<int,int> end) override; // Crop Image
+    void addFrame(Image &orig, Image* frame) override; // Adding a Frame to the Picture
+    void detectEdges(Image &orig) override; // Detect Image Edges
+    void resizeImage(Image &orig, int width, int height) override; // resize image
+    void blurImage(Image &orig, int radious) override; // Blur Image
 };
